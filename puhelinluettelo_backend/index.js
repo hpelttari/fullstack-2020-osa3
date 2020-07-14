e3e292091de45036d9d8d4c464070038f76e30cd
Hannu Pelttari <hannu.pelttari@gmail.com>
2020-07-14 15:04:14 +0300

    Tehtävä 3.5 valmis

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -24,6 +24,39 @@ let persons = [
     }
   ]
 
+app.use(express.json()) 
+
+const generateId = () => {
+  return Math.floor(Math.random()*Number.MAX_SAFE_INTEGER)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+    console.log(request.body)
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
