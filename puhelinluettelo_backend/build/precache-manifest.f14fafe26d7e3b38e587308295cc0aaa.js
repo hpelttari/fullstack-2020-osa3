99a4161b85cf92db70fa9f025a3324cbe84c165d
Hannu Pelttari <hannu.pelttari@gmail.com>
2020-07-15 12:47:32 +0300

    Tehtävä 3.11 valmis

---
+++ puhelinluettelo_backend/build/precache-manifest.f14fafe26d7e3b38e587308295cc0aaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5566e83a3f5e351d1ef3b4c3b67630",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5244fd6da7c4c6e9bb4b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bade6badc4ee71057448",
+    "url": "/static/js/2.483051de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.483051de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5244fd6da7c4c6e9bb4b",
+    "url": "/static/js/main.59ee379e.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -30,6 +30,7 @@ let persons = [
 app.use(cors())
 app.use(express.json()) 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
 
 morgan.token('body', function getBody(req) {
     if(req.method === "POST"){
