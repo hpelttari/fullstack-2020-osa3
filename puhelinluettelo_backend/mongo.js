52074480568c210e92580461898b49b91828f4b4
Hannu Pelttari <hannu.pelttari@gmail.com>
2020-07-26 12:56:15 +0300

    Tehtävä 3.12 valmis

---
+++ puhelinluettelo_backend/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+    `mongodb+srv://fullstack:${password}@cluster0.9wn4z.mongodb.net/phonebook?retryWrites=true&w=majority`
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(!name || !number){
+    console.log('phonebook:')
+    Person.find({}).then(people => {
+      people.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })
+} else{
+
+    const person= new Person({
+      name: name, 
+      number: number,
+    })
+
+    person.save().then(response => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      mongoose.connection.close()
+    })
+}
